28da59083e84c0e8d39c20f3f22accbe1b29f1be
Philippe Fernandez <mahagony4@gmail.com>
2015-09-19 18:48:18 +0200

    carriage return added

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/hi')
 def hello_world():
-    return 'Hello World!<p>Bonjour tout le monde<p>git hook added to docker'
+    return 'Hello World!\n<p>\nBonjour tout le monde\n<p>\ngit hook added to docker\n'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
