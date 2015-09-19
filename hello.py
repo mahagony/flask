f2e910baebe0c6b8d74c5fbcceabe2092a39aa97
Philippe Fernandez <mahagony4@gmail.com>
2015-09-19 19:18:07 +0200

    move to hostname printing

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+import socket
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/hi')
 def hello_world():
-    return 'Hello World!\n<p>\nBonjour tout le monde\n<p>\ngit hook added to docker\n'
+    s = 'this is not working ' + socket.gethostname()
+    return s
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
