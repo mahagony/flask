2cefaab3a42441b3c590ca7dad41a3c89c663945
Philippe Fernandez <mahagony4@gmail.com>
2015-09-19 17:38:18 +0200

    git hook to docker added

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/hi')
 def hello_world():
-    return 'Hello World!<p>Bonjour tout le monde'
+    return 'Hello World!<p>Bonjour tout le monde<p>git hook added to docker'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
